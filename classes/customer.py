f18c78be207b1a6fd5549ab7e696bd8e1ed925b0
Calum Rodger <calumrodger@gmail.com>
2022-01-12 15:14:33 +0000

    test

--- classes/customer.py
+++ classes/customer.py
@@ -1,10 +1,11 @@
 class Customer:
 
-    def __init__(self, name, wallet, age, drunkenness):
+    def __init__(self, name, wallet, age, drunkenness, fav_drink):
         self.name = name
         self.wallet = wallet
         self.age = age
         self.drunkenness = drunkenness
+        self.fav_drink = fav_drink
 
     def reduce_cash(self, drink):
         self.wallet -= drink.price
